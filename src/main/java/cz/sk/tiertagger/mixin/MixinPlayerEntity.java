813635836884fcadece5e3076d8888fed3615ea0
OndrejKozial <ondrejkozial@OKNotebok.OndrejKozial>
2026-02-19 13:43:12 +0100

    Aktualizace MixinPlayerEntity - vylepseni renderovani

--- src/main/java/cz/sk/tiertagger/mixin/MixinPlayerEntity.java
+++ src/main/java/cz/sk/tiertagger/mixin/MixinPlayerEntity.java
@@ -3,7 +3,6 @@ package cz.sk.tiertagger.mixin;
 import cz.sk.tiertagger.TierModifier;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,27 +12,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PlayerEntity.class)
 public class MixinPlayerEntity implements TierModifier {
     @Unique
-    private String czskTierSuffix = null;
+    private Text czskTierSuffix = null;
     
     @Override
-    public String getSuffix() {
+    public Text getSuffixText() {
         return czskTierSuffix;
     }
     
     @Override
-    public void setSuffix(String value) {
+    public void setSuffixText(Text value) {
         this.czskTierSuffix = value;
     }
     
     @Inject(method = "getDisplayName", at = @At("RETURN"), cancellable = true)
     private void onGetDisplayName(CallbackInfoReturnable<Text> cir) {
-        String suffix = this.czskTierSuffix;
+        Text suffix = this.czskTierSuffix;
         
-        if (suffix != null && !suffix.isEmpty()) {
+        if (suffix != null) {
             Text original = cir.getReturnValue();
             Text newText = original.copy()
-                .append(Text.literal(" " + suffix)
-                    .styled(s -> s.withColor(Formatting.GOLD)));
+                .append(Text.literal(" "))
+                .append(suffix);
             cir.setReturnValue(newText);
         }
     }
